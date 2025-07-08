5fe6d29d0cd67b0221ed7e70c5527c69297536b8
Mohil Parmar <145832606+Mohil0003@users.noreply.github.com>
2025-07-08 13:36:51 +0530

    small changes

--- Hospital_Management_System/Hospital_Management_System/obj/Debug/net8.0/Hospital_Management_System.AssemblyInfo.cs
+++ Hospital_Management_System/Hospital_Management_System/obj/Debug/net8.0/Hospital_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880b4883009d7bb4b23f69143f5e260671c2250d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
