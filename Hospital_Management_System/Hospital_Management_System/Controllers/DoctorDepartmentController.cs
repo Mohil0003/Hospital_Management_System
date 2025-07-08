880b4883009d7bb4b23f69143f5e260671c2250d
Mohil Parmar <145832606+Mohil0003@users.noreply.github.com>
2025-07-08 13:24:39 +0530

    Project

---
+++ Hospital_Management_System/Hospital_Management_System/Controllers/DoctorDepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_Management_System.Controllers
+{
+    public class DoctorDepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("DoctorDepartmentAddEdit");
+        }
+
+        public IActionResult DoctorDepartmentList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hospital_Management_System/Hospital_Management_System/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Hospital_Management_System.Controllers
+{
+
+    
+
+    public class UserController : Controller
+    {
+        private IConfiguration _configuration;
+
+         public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View("UserAddEdit");
+
+        }
+        public IActionResult UserList()
+        {
+            string ConnectionString = this._configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "[dbo].[PR_User_SelectAll]";
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            return View(table);
+        }
+
+        public IActionResult UserDelete(int UserID)
+        {
+            try
+            {
+                string connectionString = this._configuration.GetConnectionString("ConnectionString");
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "PR_User_DeleteByPK";
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = UserID;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                Console.WriteLine(ex.ToString());
+            }
+            return RedirectToAction("UserList");
+        }
+    }
+}
