0b9bec7ec528d3060ae98c94e2b40522fa133351
Mohil Parmar <145832606+Mohil0003@users.noreply.github.com>
2025-07-15 21:16:27 +0530

    SelectAll and Delete Completed

--- Hospital_Management_System/Hospital_Management_System/obj/Debug/net8.0/Hospital_Management_System.AssemblyInfo.cs
+++ Hospital_Management_System/Hospital_Management_System/obj/Debug/net8.0/Hospital_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2f3a5bcfaddb05cbbe8e6058b298f9231d8bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b915ca7f2ce35ee3367796cbd64948918ffaedf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
