aca25a7e85a3bfbec3402bcc56bc9c91d9c18617
Mohil Parmar <145832606+Mohil0003@users.noreply.github.com>
2025-08-14 20:23:50 +0530

    icons changes

--- Hospital_Management_System/Hospital_Management_System/obj/Debug/net8.0/Hospital_Management_System.AssemblyInfo.cs
+++ Hospital_Management_System/Hospital_Management_System/obj/Debug/net8.0/Hospital_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e10bfc242dea27abdabaebf8f856a1a93b4c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74c63a3f37d3bbcdd2dcdfb7a53f0abd1518271")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
